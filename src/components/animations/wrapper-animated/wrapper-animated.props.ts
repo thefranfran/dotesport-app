a5df8a0c46bae647719c229bfcf527336b6c2cb1
francoiscipriani <francoiscipriani@gmail.com>
2024-10-09 18:46:51 +0200

    feat(animations): making a wrapper animation

---
+++ src/components/animations/wrapper-animated/wrapper-animated.props.ts
@@ -0,0 +1,10 @@
+export interface WrapperAnimatedProps extends React.PropsWithChildren {
+    /**
+     * The delay before the animation starts
+     */
+    delay?: number;
+    /**
+     * The duration of the animation
+     */
+    duration?: number;
+}
